57fc57b513a7f38f3c7742f0d337b4c8e9bf3efc
PabPol <pabpolval@gmail.com>
2023-09-11 00:58:15 -0300

    add navbar toogle button

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,12 +5,28 @@ export default (project) => {
     const header = document.createElement("header");
     const projectTitle = document.createElement("h2");
     const projectDescription = document.createElement("p");
+    const toggleNav = document.createElement("div");
+    const navIcon = document.createElement("i");
+
     header.className = "header";
     projectTitle.className = "project-title";
+    toggleNav.className = "toggle";
+    navIcon.className = "fa fa-navicon";
 
     projectTitle.textContent = project.title;
     projectDescription.textContent = project.description;
+    
+    toggleNav.addEventListener('click', () => {
+        const navigation = document.querySelector(".navigation");
+        console.log(navigation);
+        main.classList.toggle('active');
+        navigation.classList.toggle('active');
+    });
 
+    
+    toggleNav.appendChild(navIcon);
+
+    header.appendChild(toggleNav);
     header.appendChild(projectTitle);
     header.appendChild(projectDescription);
     return header;
