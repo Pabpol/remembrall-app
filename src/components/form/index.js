b2407871efb68ed0a4425d668862629a6aa78773
PabPol <pabpolval@gmail.com>
2023-09-16 14:28:54 -0300

    add new todo and project form

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,11 +1,13 @@
+import Todo from "../../classes/Todo";
+import Project from "../../classes/Project";
 import "./index.styl";
 
 
 export default (type) => {
     const main = document.getElementById("main");
-    const modal = document.createElement('section');
-    const modalContent = document.createElement('section');
-    const form = type === "todoForm"? createTodoForm():createProjectForm();
+    const modal = document.createElement("section");
+    const modalContent = document.createElement("section");
+    const form = type === "todoForm" ? createTodoForm() : createProjectForm();
 
 
     modal.className = "modal-overlay";
@@ -19,61 +21,64 @@ export default (type) => {
 }
 
 function createTodoForm() {
-    const form = document.createElement('form');
-    form.className = 'todo-form';
+    const form = document.createElement("form");
+    form.className = "todo-form";
 
     // Create title field
-    const titleSpan = document.createElement('span');
-    titleSpan.className = 'input-span';
-    const titleLabel = document.createElement('label');
-    titleLabel.setAttribute('for', 'title');
-    titleLabel.className = 'label';
-    titleLabel.textContent = 'Title';
-    const titleInput = document.createElement('input');
-    titleInput.setAttribute('type', 'text');
-    titleInput.setAttribute('name', 'title');
-    titleInput.setAttribute('id', 'title');
+    const titleSpan = document.createElement("span");
+    titleSpan.className = "input-span";
+    const titleLabel = document.createElement("label");
+    titleLabel.setAttribute("for", "title");
+    titleLabel.className = "label";
+    titleLabel.textContent = "Title";
+    const titleInput = document.createElement("input");
+    titleInput.setAttribute("type", "text");
+    titleInput.setAttribute("name", "title");
+    titleInput.setAttribute("id", "title");
     titleSpan.appendChild(titleLabel);
     titleSpan.appendChild(titleInput);
 
     // Create description field
-    const descSpan = document.createElement('span');
-    descSpan.className = 'input-span';
-    const descLabel = document.createElement('label');
-    descLabel.setAttribute('for', 'description');
-    descLabel.className = 'label';
-    descLabel.textContent = 'Description';
-    const descInput = document.createElement('textarea');
-    descInput.setAttribute('name', 'description');
-    descInput.setAttribute('id', 'description');
+    const descSpan = document.createElement("span");
+    descSpan.className = "input-span";
+    const descLabel = document.createElement("label");
+    descLabel.setAttribute("for", "description");
+    descLabel.className = "label";
+    descLabel.textContent = "Description";
+    const descInput = document.createElement("textarea");
+    descInput.setAttribute("name", "description");
+    descInput.setAttribute("id", "description");
     descSpan.appendChild(descLabel);
     descSpan.appendChild(descInput);
 
     // Create due date field
-    const dateSpan = document.createElement('span');
-    dateSpan.className = 'input-span';
-    const dateLabel = document.createElement('label');
-    dateLabel.setAttribute('for', 'dueDate');
-    dateLabel.className = 'label';
-    dateLabel.textContent = 'Due Date';
-    const dateInput = document.createElement('input');
-    dateInput.setAttribute('type', 'date');
-    dateInput.setAttribute('name', 'dueDate');
-    dateInput.setAttribute('id', 'dueDate');
+    const dateSpan = document.createElement("span");
+    dateSpan.className = "input-span";
+    const dateLabel = document.createElement("label");
+    dateLabel.setAttribute("for", "dueDate");
+    dateLabel.className = "label";
+    dateLabel.textContent = "Due Date";
+    const dateInput = document.createElement("input");
+    dateInput.setAttribute("type", "date");
+    dateInput.setAttribute("name", "dueDate");
+    dateInput.setAttribute("id", "dueDate");
     dateSpan.appendChild(dateLabel);
     dateSpan.appendChild(dateInput);
 
     // Create submit button
-    const submitInput = document.createElement('input');
-    submitInput.className = 'submit';
-    submitInput.setAttribute('type', 'submit');
-    submitInput.setAttribute('value', 'Add Todo');
+    const submitInput = document.createElement("input");
+    submitInput.className = "submit";
+    submitInput.setAttribute("type", "submit");
+    submitInput.textContent = "Add To-Do"
+    submitInput.addEventListener("click", (e) => {
+        addTodo(titleInput.value, descInput.value, dateInput.value);
+    });
 
     // Create close icon
-    const closeIcon = document.createElement('i');
-    closeIcon.className = 'fa fa-times close-icon';
-    closeIcon.addEventListener('click', () => {
-        const modal  = document.querySelector(".modal-overlay")
+    const closeIcon = document.createElement("i");
+    closeIcon.className = "fa fa-times close-icon";
+    closeIcon.addEventListener("click", () => {
+        const modal = document.querySelector(".modal-overlay")
         modal.remove();
     });
 
@@ -88,56 +93,72 @@ function createTodoForm() {
 }
 
 function createProjectForm() {
-    const form = document.createElement('form');
-    form.className = 'todo-form';
-  
+    const form = document.createElement("form");
+    form.className = "todo-form";
+
     // Create title field
-    const titleSpan = document.createElement('span');
-    titleSpan.className = 'input-span';
-    const titleLabel = document.createElement('label');
-    titleLabel.setAttribute('for', 'projectTitle');
-    titleLabel.className = 'label';
-    titleLabel.textContent = 'Project Title';
-    const titleInput = document.createElement('input');
-    titleInput.setAttribute('type', 'text');
-    titleInput.setAttribute('name', 'projectTitle');
-    titleInput.setAttribute('id', 'projectTitle');
+    const titleSpan = document.createElement("span");
+    titleSpan.className = "input-span";
+    const titleLabel = document.createElement("label");
+    titleLabel.setAttribute("for", "projectTitle");
+    titleLabel.className = "label";
+    titleLabel.textContent = "Project Title";
+    const titleInput = document.createElement("input");
+    titleInput.setAttribute("type", "text");
+    titleInput.setAttribute("name", "projectTitle");
+    titleInput.setAttribute("id", "projectTitle");
     titleSpan.appendChild(titleLabel);
     titleSpan.appendChild(titleInput);
-  
+
     // Create description field
-    const descSpan = document.createElement('span');
-    descSpan.className = 'input-span';
-    const descLabel = document.createElement('label');
-    descLabel.setAttribute('for', 'projectDescription');
-    descLabel.className = 'label';
-    descLabel.textContent = 'Project Description';
-    const descInput = document.createElement('textarea');
-    descInput.setAttribute('name', 'projectDescription');
-    descInput.setAttribute('id', 'projectDescription');
+    const descSpan = document.createElement("span");
+    descSpan.className = "input-span";
+    const descLabel = document.createElement("label");
+    descLabel.setAttribute("for", "projectDescription");
+    descLabel.className = "label";
+    descLabel.textContent = "Project Description";
+    const descInput = document.createElement("textarea");
+    descInput.setAttribute("name", "projectDescription");
+    descInput.setAttribute("id", "projectDescription");
     descSpan.appendChild(descLabel);
     descSpan.appendChild(descInput);
-  
+
     // Create close icon
-    const closeIcon = document.createElement('i');
-    closeIcon.className = 'fa fa-times close-icon';
-    closeIcon.addEventListener('click', () => {
-        const modal  = document.querySelector(".modal-overlay")
+    const closeIcon = document.createElement("i");
+    closeIcon.className = "fa fa-times close-icon";
+    closeIcon.addEventListener("click", () => {
+        const modal = document.querySelector(".modal-overlay")
         modal.remove();
     });
-  
+
     // Create submit button
-    const submitInput = document.createElement('input');
-    submitInput.className = 'submit';
-    submitInput.setAttribute('type', 'submit');
-    submitInput.setAttribute('value', 'Add Project');
-  
+    const submitInput = document.createElement("input");
+    submitInput.className = "submit";
+    submitInput.setAttribute("type", "submit");
+    submitInput.textContent = "Add Project"
+    submitInput.addEventListener("click", () => {
+        addProject(titleInput.value, descInput.value);
+    });
     // Append close icon and all fields to form
     form.appendChild(closeIcon);
     form.appendChild(titleSpan);
     form.appendChild(descSpan);
     form.appendChild(submitInput);
-  
+
     return form;
-  }
-  
+}
+
+function addTodo(title, description, dueDate) {
+    const existingTodos = JSON.parse(localStorage.getItem('todos')) || [];
+    const todo = new Todo(existingTodos.todos.length+1, title, description, dueDate)
+    existingTodos.todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(existingTodos));
+
+}
+function addProject(title, description) {
+    const existingProjects = JSON.parse(localStorage.getItem('projects')) || [];
+    const project = new Project(existingProjects.projects.length+1, title, description)
+    existingProjects.projects.push(project);
+    localStorage.setItem('projects', JSON.stringify(existingProjects));
+
+}
