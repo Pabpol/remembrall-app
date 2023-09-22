46c23ec2f599efc7bd0447be0e35abc6c7a84457
PabPol <pabpolval@gmail.com>
2023-09-21 23:03:40 -0300

    fix: Update title of To-Do list

--- src/components/cardList/index.js
+++ src/components/cardList/index.js
@@ -4,16 +4,14 @@ import card from "../cards/index"
 
 export default (arrayTodo) => {
     const activeProjectId = parseInt(localStorage.getItem("activeProjectId"));
-    console.log(activeProjectId);
     const filteredTodos = arrayTodo.filter(todo => todo.projectId == activeProjectId);
-    console.log(filteredTodos);
     const todos = document.createElement("div");
     const todoList = document.createElement("div");
     const listTitle = document.createElement("div");
     todos.className = "todos";
     todoList.className = "todoList";
     listTitle.className = "listTitle";
-    listTitle.textContent = "To do"
+    listTitle.textContent = "To-Do List"
 
     filteredTodos.forEach(todo => {
         const cardElement = card(todo);
